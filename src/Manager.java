0bb80232d72ed8b5d973d870b4c21212d080a3c4
DIRM2705 <dirm2705@gmail.com>
2024-10-09 09:47:23 -0600

    Añade documentacion

--- src/Manager.java
+++ src/Manager.java
@@ -3,19 +3,32 @@ public class Manager
     private InterfazDeUsuario iu;
     private Jugador[] jugadores;
 
+    //Constructor de clase
     public Manager()
     {
+        /*
+        * Se crean las pantallas a través de la interfaz de usuario
+        * Se crean los jugadores sin valores iniciales
+        */
         iu = new InterfazDeUsuario();
         jugadores = new Jugador[2];
     }
 
     public void jugar()
     {
+        /*
+        * Llama a la interfaz de usuario a comenzar con el Nivel 1
+         */
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public void mostrarInfoJugador(Jugador jugador)
     {
+        /*
+        * Muestra la información del jugador seleccionado
+        *
+        * @param jugador:  Jugador a mostrar
+         */
         System.out.println("Nombre: " + jugador.getNombre());
         System.out.println("Puntaje: " + jugador.getPuntaje());
         System.out.println("Vidas: " + jugador.getVidas());
@@ -23,11 +36,18 @@ public class Manager
 
     public void procesarRespuesta()
     {
+        /*
+        * Revisa si la respuesta del jugador es correcta o no
+         */
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public void irASiguienteNivel()
     {
+        /*
+        * Revisa si el jugador cuenta con los requisitos para avanzar de nivel
+        * Si es así, llama a la interfaz de usuario a avanzar de nivel
+         */
         throw new UnsupportedOperationException("Not supported yet.");
     }
 }
