3981cbcf97c625c329237c07490bbab4d8e67967
Jouhg <159324901+Jouhg@users.noreply.github.com>
2024-10-17 21:42:39 -0600

    Add files via upload

--- src/InterfazDeUsuario.java
+++ src/InterfazDeUsuario.java
@@ -6,35 +6,54 @@ public class InterfazDeUsuario
     public InterfazDeUsuario()
     {
         //TODO: CREAR EL RESTO DE PANTALLAS
+        pantallas = new Pantalla[]
+                {
+                        new Menu("Bienvenid@ al juego Vita Terra\n1.Jugar\n2.Salir", null),
+                        new Menu("Elige el jugador que queires ser:\n-Jugador 1\n-Jugador 2 ",null),
+                        new Pantalla("Intrucciones:\n 1. Se presentan por nivel 10 desechos a clasficar" +
+                                "dentro del contenedor correcto. Por cada desecho clasificado correctamente" +
+                                "se obtendrá una cierta cantidad de puntos y por cada desecho clasificado" +
+                                "incorrectamente se pierde una cierta cantidad de vidas; estas cantidades" +
+                                "dependen del nivel en el que se encuentre el jugador.\n2. Al conseguir clasificar" +
+                                "el mínimo de desechos que tiene cada nivel, se pasa a una segunda etapa que es tratar" +
+                                "estos desechos en la planta tratadora. En esta etapa muestran los pasos de cómo tratar" +
+                                "cada desecho y se tiene que eligir el orden correcto en el que van estos pasos.\n 3. Si" +
+                                "el jugador trata correctamente la cantidad mínima de desechos del nivel, pasa al siguiente nivel" +
+                                "y si es el último nivel gana el juego.\n4. El juego termina cuando se han clasificado todos" +
+                                "los desechos o cuando el jugador pierde todas sus vidas.",null),
+                        new Menu("Datos del jugador: {0}\nDesecho a clasificar: {1}\n¿En qué contenedor debes meter el desecho?\n" +
+                                "Elige un contenedor de los siguientes:\n{2}"),
+                        new Pantalla("¡Muy bien!, has clasificado correctamente la cantidad mínima de desechos para este nivel.\n" +
+                                "Ahora continuamos llevando estos desechos a la Planta Tratadora"),
+                        new Menu("Datos del jugador: {0}\nDesecho a tratar: {1}\n¿Cómo se trata este desecho?\n{2}"),
+                        new Menu("¡Correcto!\nAhora organiza los pasos a seguir para tratar el desecho:\n{0}"),
+                        new Pantalla("Incorrecto"),
+                        new Pantalla("Fin del juego, perdiste"),
+                        new Pantalla ("¡Felicidades!, {0} ganado")
+                }
+
+
         pantallaActual = 0;
     }
 
-    public Pantalla cambiarAPantalla(int pantalla)
+    public void cambiarAPantalla(int pantalla)
     {
-        pantallaActual = pantalla;
+        pantallaActual = 0;
         mostrarPantalla();
-        return pantallas[pantalla];
-    }
-
-    public void actualizarPantalla(int pantalla, String[] argumentos)
-    {
-        Pantalla pantallaActualizar = pantallas[pantalla];
-        if(pantallaActualizar.getCantidadArgumentos() <= argumentos.length)
-        {
-            for (int i = 0; i < argumentos.length; i++)
-            {
-                pantallaActualizar.setArgumento(i, argumentos[i]);
-            }
-        }
     }
 
     private void mostrarPantalla()
     {
         borrarPantalla();
-        pantallas[pantallaActual].mostrar();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    private void borrarPantalla() {
-        System.out.println("\u000C");
+    private void borrarPantalla()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int getPantallaActual() {
+        return pantallaActual;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ public class Main
     public static void main(String[] args) {
         //Crea el manager
         Manager juego = new Manager();
+
+        //Instacias de nivel
+        Nivel[] niveles=new Nivel[]{
+                new Nivel(10, 1, Contenedor[] contenedores, 7, juego, 130),
+                new Nivel(15, 1, Contenedor[] contenedores, 8, juego, 110),
+                new Nivel(20, 2, Contenedor[] contenedores, 9, juego, 100)
+        }
     }
 }
