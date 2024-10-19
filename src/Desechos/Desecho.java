13baa07e3967939c7bf651718f0bb3456e0fe924
TaniaGarNi <159318034+TaniaGarNi@users.noreply.github.com>
2024-10-18 19:05:44 -0600

    Add files via upload
    Crea desechos

---
+++ src/Desechos/Desecho.java
@@ -0,0 +1,13 @@
+public abstract class Desecho
+{
+    private String nombre;
+    private String clasificacion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+}
