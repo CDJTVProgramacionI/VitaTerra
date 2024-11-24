9468b73c0f5a991946819c141464c11151222df2
camilachida06 <159325528+camilachida06@users.noreply.github.com>
2024-11-23 23:03:35 -0600

    Add files via upload

---
+++ src/manejoArchivos.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class manejoArchivos {
+
+    //Atributos
+    private String nombreArchivo;
+
+    //Constructor
+    public manejoArchivos (String nombreArchivo){
+        this.nombreArchivo = nombreArchivo;
+    }
+
+    //Métodos
+    public void escribirDatos(Jugador jugador) {
+        try (BufferedWriter br = new BufferedWriter(new FileWriter(nombreArchivo, true))) {
+            br.write(jugador.getInfo());
+            br.newLine();
+        } catch (IOException exc) {
+            System.err.println("Error al escribir en el archivo: " + exc.getMessage());
+        }
+    }
+}
