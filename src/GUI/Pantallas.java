e78cac419a2888c8c5c998d1a19c9640f1d6e61d
DIRM2705 <dirm2705@gmail.com>
2024-11-24 10:30:43 -0600

    Añade pantallas de GUI

---
+++ src/GUI/Pantallas.java
@@ -0,0 +1,21 @@
+package GUI;
+
+import javax.swing.*;
+
+public abstract class Pantallas extends JFrame {
+    protected JPanel panelPrincipal;
+
+    public Pantallas() {
+        setTitle("Ventana");
+        setSize(700, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panelPrincipal = new JPanel();
+        add(panelPrincipal);
+
+        inicializar();
+        setVisible(true);
+    }
+
+    protected abstract void inicializar();
+}
